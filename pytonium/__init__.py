d3d86a678ba677a9657faf09aa332a57bb662058
Nick Palenchar <nickpal@nickpalenchar.com>
2020-02-11 11:38:54 -0500

    config update methods

--- pytonium/__init__.py
+++ pytonium/__init__.py
@@ -11,6 +11,8 @@ from .options import Config
 from unittest import TestLoader, TextTestRunner
 from xmlrunner import XMLTestRunner
 from os import path
+import logging
+import json
 
 # from selenium.webdriver.common import keys as keys
 
@@ -41,3 +43,40 @@ def main(pattern='test*.py', config=None, argv=None, start_directory='.', config
     runner.run(tests)
 
     pprint(config_options.options)
+
+
+def resolve_config_arguments():
+    config = Config()
+    update_config_from_environment_variables(config)
+
+
+def update_config_from_environment_variables(config: Config):
+    config_environment_variables = map(lambda x: f'KR_{x.upper()}', config.DEFAULT_OPTIONS.keys())
+
+    for env in config_environment_variables:
+        value = os.environ.get(env)
+        if value is not None:
+            try:
+                key = env.replace('KR_', '').lower()
+                Config.DEFAULT_OPTIONS[key] = value
+            except KeyError:
+                logging.warning(f'KR_WARNING: Config option {key} tried to be set from environment variable {env} but is not a valid option. Skipping.')
+
+
+def update_config_from_file(file):
+    pass
+
+def update_config_form_json(config: Config, jsn: str):
+    args = json.loads(jsn)
+    update_config_from_dict(args)
+
+def update_config_from_args(config: Config, args: str):
+    _args = map(lambda x: x.split(','), args.split('='))
+    update_config_from_dict(dict(_args))
+
+def update_config_from_dict(config: Config, dictionary):
+    for key, value in dictionary:
+        try:
+            config.options[key] = value
+        except KeyError:
+            logging.warning(f'KR_WARNING: Config option {key} tried to be set but is not a valid option. Skipping')
